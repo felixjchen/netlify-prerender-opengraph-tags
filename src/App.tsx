8922eee7326bbc1b2a12a03dcb91a4587ef30eea
felixjchen <31393977+felixjchen@users.noreply.github.com>
2021-11-30 00:24:11 -0500

    images

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import pinapple from "./pinapple.jpg";
+import blanket from "./blanket.jpg";
+import "./App.css";
+import { useParams } from "react-router";
+
+import { BrowserRouter, Link, Outlet, Routes, Route } from "react-router-dom";
+
+const Photo = () => {
+  const { name } = useParams();
+  if (name === undefined) return <> </>;
+
+  const image = { pinapple, blanket }[name];
+
+  return (
+    <>
+      <img src={image} className="App-logo" alt="logo" />{" "}
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <nav
+            style={{
+              borderBottom: "solid 1px",
+              paddingBottom: "1rem",
+            }}
+          >
+            <Link to="/">Home</Link>
+            <Link to="/photo/pinapple">PINAPPLE</Link>
+            <Link to="/photo/blanket">BLANKET</Link>
+          </nav>
+          <Outlet />
+          <Routes>
+            <Route
+              path="/"
+              element={<img src={logo} className="App-logo" alt="logo" />}
+            />
+            <Route path="/photo/:name" element={<Photo />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
